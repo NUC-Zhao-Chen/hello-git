875e379ed682faee0f090297d516b02d7a559259
NUC-Zhao-Chen <1040101027@qq.com>
2020-10-21 10:13:30 +0800

    第一次使用idea提交代码

---
+++ myIdeaTest/src/cn/edu/nuc/demo1/Demo1.java
@@ -0,0 +1,11 @@
+package cn.edu.nuc.demo1;
+import java.util.Scanner;
+public class Demo1 {
+    public static void main(String[] args) {
+        double a = 1.00;
+        System.out.println(a);
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        System.out.println(i);
+    }
+}
